a026f8614c6756f9075f4c1833a0bbc333a2a595
Prasanth <55478056+prasanth132910@users.noreply.github.com>
2020-06-19 23:37:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function Greetings(){
+    var front = document.getElementById("front");
+    front.style.visibility="hidden";
+    var back = document.getElementById("back");
+    back.style.visibility = "visible";
+  }
+  
